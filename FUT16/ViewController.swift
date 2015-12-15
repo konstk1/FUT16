93909b60342f4ef213e43bb97594d0c27adde5f7
Konstantin Klitenik <konstantin@bytelight.com>
2015-12-15 16:02:23 -0500

    Initial commit

---
+++ FUT16/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FUT16
+//
+//  Created by Konstantin Klitenik on 12/15/15.
+//  Copyright © 2015 Kon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
