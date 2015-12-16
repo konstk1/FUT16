b06bb23c0dbb2825cff6a7c6e00c3151cb1d2c5c
Konstantin Klitenik <konstantin@bytelight.com>
2015-12-16 17:33:03 -0500

    Login working.

--- FUT16/FUT16.swift
+++ FUT16/FUT16.swift
@@ -15,6 +15,7 @@ class FUT16 {
     let cookieStoreage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
     let alamo: Manager!
     
+    var loginUrl: URLStringConvertible!
     let jSessionCookieUrl = NSURL(string: "https://signin.ea.com/p/JSESSIONID")!
 
     init() {
@@ -22,48 +23,53 @@ class FUT16 {
         cfg.HTTPCookieAcceptPolicy = NSHTTPCookieAcceptPolicy.Always
 
         alamo = Alamofire.Manager.sharedInstance
-        
-        getLoginUrl()
     }
     
-    func getLoginUrl() {
+    func login(email: String, password: String) {
         let url = "https://www.easports.com/fifa/ultimate-team/web-app"
-        let headers = ["Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-                       "Accept-Encoding" : "gzip, deflate, sdch",
-                        "Accept-Language":"en-US,en;q=0.8",
-                        "Connection" : "keep-alive",
-                        "Host" : "www.easports.com",
-                        "Upgrade-Insecure-Requests" : "1",
-                        "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36",
-                        "X-FirePHP-Version" : "0.0.6"]
         
-        alamo.request(.GET, url, headers: headers).response { (request, response, data, error) -> Void in
-            print(response)
-            self.printCookies()
-            self.sendUsernamePassword(response!.URL!)
-        }        
+        alamo.request(.GET, url).response { (request, response, data, error) -> Void in
+            self.loginUrl = response!.URL!
+            if self.loginUrl.URLString.containsString("web-app") {
+                print("Already Logged In.")
+                self.printCookies()
+            } else {
+                self.sendUsernamePassword(email, password: password)
+            }
+        }
     }
     
-    func sendUsernamePassword(url: URLStringConvertible) {
-        guard let cookies = cookieStoreage.cookiesForURL(jSessionCookieUrl) else {
-            print("JSESSIONID Cookie not found")
-            return
-        }
-        
-        var headers = ["Content-Type" : "application/x-www-form-urlencoded"
-            Cookie:JSESSIONID=4DFAB11EDD64680DAB4E3AC447B4AD0E.eanshprdaccounts35; utag_main=_st:1450287259431$ses_id:1450286325763%3Bexp-session; __utma=103303007.221262541.1450285459.1450285459.1450285459.1; __utmc=103303007; __utmz=103303007.1450285459.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)
-            Host:signin.ea.com
-        var headers = NSHTTPCookie.requestHeaderFieldsWithCookies(cookies)
-        
-        let parameters = ["email"    : "kostyan5@gmail.com",
-                          "password" : "test",
+    private func sendUsernamePassword(email: String, password: String) {
+        let parameters = ["email"    : email,
+                          "password" : password,
                            "_eventId" : "submit"]
         
-        alamo.request(.POST, url, parameters: parameters, headers: headers).response { (request, response, data, error) -> Void in
-            print("Response: \(response)")
-            self.printCookies()
+        alamo.request(.POST, loginUrl, parameters: parameters).response { (request, response, data, error) -> Void in
+            if let responseString = String(data: data!, encoding: NSUTF8StringEncoding) {
+                print(responseString)
+                if responseString.containsString("Submit Security Code") {
+                    print("Enter Security Code!")
+                    // update login URL to sequence 2
+                    self.loginUrl = response!.URL!
+                } else {
+                    print("Invalid login!")
+                }
+            }
         }
     }
+    
+    func sendAuthCode(authCode: String) {
+        let parameters = ["twofactorCode" : authCode,
+                          "_trustThisDevice" : "on",
+                          "trustThisDevice" : "on",
+                          "_eventId" : "submit"]
+        
+        alamo.request(.POST, loginUrl, parameters: parameters).response {(request, response, data, error) -> Void in
+            print(response)
+            print(String(data: data!, encoding: NSUTF8StringEncoding))
+        }
+    }
+    
     
     private func printCookies() {
         guard let cookies = cookieStoreage.cookies else {
--- FUT16/ViewController.swift
+++ FUT16/ViewController.swift
@@ -10,10 +10,21 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var emailTextField: NSTextField!
+    @IBOutlet weak var passwordTextField: NSSecureTextField!
+    @IBOutlet weak var authTextField: NSTextField!
+    
+    
+    @IBOutlet weak var loginButton: NSButton!
+    @IBOutlet weak var submitButton: NSButton!
+    
+    private let fut16 = FUT16()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
-        let fut16 = FUT16()
+        loadSavedSettings()
     }
 
     override var representedObject: AnyObject? {
@@ -22,6 +33,26 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func loginPressed(sender: NSButton) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setValue(emailTextField.stringValue, forKey: "ea-email")
+        defaults.setValue(passwordTextField.stringValue, forKey: "ea-password")
+        
+        fut16.login(emailTextField.stringValue, password: passwordTextField.stringValue)
+    }
+    
+    @IBAction func submitPressed(sender: NSButton) {
+        fut16.sendAuthCode(authTextField.stringValue)
+    }
+    
+    private func loadSavedSettings() {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        if let email = defaults.valueForKey("ea-email") as? String {
+            emailTextField.stringValue = email
+        }
+        if let pass = defaults.valueForKey("ea-password") as? String {
+            passwordTextField.stringValue = pass
+        }
+    }
 }
 
